9679c9bffcfa50b3f9730a540ec2ece23eedd3f2
Colin Burgin <cburgin707@gmail.com>
2017-01-09 12:23:33 -0500

    Can parse recipe file, need to generate HTML. Created template recipe file.  Need to create template recipe html.

--- python/addRecipe.py
+++ python/addRecipe.py
@@ -2,13 +2,47 @@
 
 # Class that is capable of parsing a recipe file and addeding it to the webkit-justify-content
 
-import configparser
+import os
 
 class addRecipe:
     """Adds recipe to the site"""
     def __init__(self, filename):
-        pass
+        # Create recipe attributes
+        self.recipe = None
+        self.description = None
+        self.image = None
+        self.ingredients = None
+        self.instructions = None
+        self.nutrition = None
 
-    #Uses configparser to parse a recipe and store it in an intermediate format
-    def parse_recipe(self):
-        config = configparser.ConfigParser()
+        self.parse_recipe(filename)
+
+    # Parse the recipe file
+    def parse_recipe(self, filename):
+        # Open and read the entire file
+        with open(filename, "r") as f:
+            recipe = f.read()
+            # Split the recipe into sections and parse each section
+            for sections in recipe.split(os.linesep + os.linesep):
+                lines = sections.split(os.linesep)
+                if lines[0] == "[RECIPE]":
+                    self.recipe = lines[1:]
+                elif lines[0] == "[DESCRIPTION]":
+                    self.description = lines[1:]
+                elif lines[0] == "[IMAGE]":
+                    self.image = lines[1:]
+                elif lines[0] == "[INGREDIENTS]":
+                    self.ingredients = lines[1:]
+                elif lines[0] == "[INSTRUCTIONS]":
+                    self.instructions = lines[1:]
+                elif lines[0] == "[NUTRITION]":
+                    self.nutrition = lines[1:]
+
+    # Convert the recipe into an html file
+    def write_recipe(self, filename):
+
+def main():
+    recipe = addRecipe("eggroll.txt")
+
+if __name__ == "__main__":
+    main()
